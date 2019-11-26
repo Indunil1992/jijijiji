a0b91c096f18cbdea49e2830d52ae276bb44d358
Indunil1992 <indunil@adroitlogic.com>
2019-11-26 17:56:10 +0530

    1

--- jijijiji/ConnectionManager.js
+++ jijijiji/ConnectionManager.js
@@ -1,18 +1,11 @@
 module.exports = function() {
     this.dbConnections = [];
 
-    this.dbConnections["hhh"] = {
-        host: process.env.EndPoint_rdsHhh,
-        port: process.env.Port_rdsHhh,
-        user: process.env.User_rdsHhh,
-        password: process.env.Password_rdsHhh,
-        database: "ccc"
-    };
-    this.dbConnections["uiuijjj"] = {
-        host: process.env.EndPoint_rdsUiuijjj,
-        port: process.env.Port_rdsUiuijjj,
-        user: process.env.User_rdsUiuijjj,
-        password: process.env.Password_rdsUiuijjj,
-        database: "jiu"
+    this.dbConnections["indunil"] = {
+        host: process.env.EndPoint_rdsIndunil,
+        port: process.env.Port_rdsIndunil,
+        user: process.env.User_rdsIndunil,
+        password: process.env.Password_rdsIndunil,
+        database: "induRDS"
     };
 };
--- jijijiji/lambda.js
+++ jijijiji/lambda.js
@@ -1,33 +1,12 @@
 let SL_AWS = require('slappforge-sdk-aws');
 let connectionManager = require('./ConnectionManager');
 const rds = new SL_AWS.RDS(connectionManager);
-let AWS = require('aws-sdk');
-const rekognition = new AWS.Rekognition();
 
 exports.handler = function (event, context, callback) {
 
-    // You can pass the existing connection to this function.
-    // A new connection will be created if it's not present as the third param 
-    // You must always end/destroy the DB connection after it's used
-    rds.query({
-        instanceIdentifier: 'hhh',
-        query: 'name, age',
-        inserts: ["name"]
-    }, function (error, results, connection) {
-        if (error) {
-            console.log("Error occurred");
-            throw error;
-        } else {
-            console.log("Success")
-            console.log(results);
-        }
-
-        connection.end();
-    });
-
     // You must always end/destroy the DB connection after it's used
     rds.beginTransaction({
-        instanceIdentifier: 'uiuijjj'
+        instanceIdentifier: 'indunil'
     }, function (error, connection) {
         if (error) {
             throw error;
